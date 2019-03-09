b2ef68d5ce6b507b7ef9235f720aa789715d62d2
Daniele <daniele.tortora@hackney.gov.uk>
2019-03-09 13:35:51 +0000

    flatten array to determine if there are 0 ingredients in total in children component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey+i} type={igKey}/>
-        })
+  let transformedIngredients = Object.keys(props.ingredients) // [salad, meat...]
+    .map(ingredientName => {
+      return [...Array(props.ingredients[ingredientName])].map((_, i) => {
+        //Make array of element which count is exact equals to number of ingredient for adding. Next, iterate and generate ingredients.
+        return <BurgerIngredient type={ingredientName} key={i} />;
+      });
+    })
+    .filter(singleIngredientArray => {
+      return singleIngredientArray.length > 0;
     });
-
-    return (
+  // console.log(transformedIngredients) // for testing purposes
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
       {transformedIngredients}
