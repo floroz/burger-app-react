562b8ebfecf4a9bab09399c381ce41e5c7be0769
Daniele <daniele.tortora@hackney.gov.uk>
2019-03-10 15:26:45 +0000

    toolbar set up

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = props => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button clicked={props.purchaseCancelled} btnType='Success'>CANCEL</Button>
             <Button clicked={props.purchaseContinued} btnType='Danger'>CONTINUE</Button>
