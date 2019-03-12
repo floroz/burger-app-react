e997aac45e3b951a8deb00abbf67094972689d04
Daniele <daniele.tortora@hackney.gov.uk>
2019-03-12 10:53:30 +0000

    use cycle hooks to prevent order summary to update unnecessarily

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] will update');
+    }
+    
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(ingKey => {
+            return (
+                <li key={ingKey}>
+                <span style={{textTransform: 'capitalize'}}>
+                {ingKey}</span>: {this.props.ingredients[ingKey]}
+                </li>
+            )
+        })
         return (
-            <li key={ingKey}>
-            <span style={{textTransform: 'capitalize'}}>
-            {ingKey}</span>: {props.ingredients[ingKey]}
-            </li>
-        )
-    })
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Delicious Burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button clicked={props.purchaseCancelled} btnType='Success'>CANCEL</Button>
-            <Button clicked={props.purchaseContinued} btnType='Danger'>CONTINUE</Button>
-        </Aux>
-    );
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Delicious Burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button clicked={this.props.purchaseCancelled} btnType='Success'>CANCEL</Button>
+                <Button clicked={this.props.purchaseContinued} btnType='Danger'>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
