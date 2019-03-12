238922cb9c3bfb0024a5042079f93f57624d6d95
Daniele <daniele.tortora@hackney.gov.uk>
2019-03-12 10:29:58 +0000

    logo and toggledrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import classes from './Toolbar.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems'
+import React from "react";
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "./DrawerToggle/DrawerToggle";
 
 const toolbar = props => (
-    <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
-           <NavigationItems/>
-        </nav>
-    </header>
-)
+  <header className={classes.Toolbar}>
+    <DrawerToggle open={props.open} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
 export default toolbar;
