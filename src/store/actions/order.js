53e03a97740b732e4f2a6457e74f52a012b44d2d
Daniele <daniele.tortora@hackney.gov.uk>
2019-04-13 12:11:16 +0100

    order management

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -41,3 +41,44 @@ export const purchaseInit = () => {
     type: actionTypes.PURCHASE_INIT
   };
 };
+
+export const fetchOrdersSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrderFail = error => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrderStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrderStart());
+    axios
+      .get("/orders.json")
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrderFail(err));
+      });
+  };
+};
